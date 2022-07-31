db111aaa3cb2ed67531cbd57e2ce5de5d07d71f9
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-07-31 20:36:02 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css';
+
+function App() {
+
+  return (
+    <>
+      <p>todo</p>
+
+      <input type="text" placeholder='Adicione uma nova tarefa' />
+      <button>Criar +</button>
+    </>
+  );
+}
+
+export default App
