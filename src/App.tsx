a94599c0614e8e54a13274917d9f4ac16ef760b7
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-08-03 22:07:35 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { Logo } from './components/Logo';
 import './global.css';
 
 function App() {
 
   return (
     <>
-      <p>todo</p>
+      <Logo />
 
-      <input type="text" placeholder='Adicione uma nova tarefa' />
-      <button>Criar +</button>
+      <div className='tasks'>
+        <input type="text" placeholder='Adicione uma nova tarefa' />
+        <button>Criar +</button>
+      </div>
+
+      <div className='taskList'>
+        <a href="#">Tarefas criadas 0</a>
+        <a href="#">Concluídas 0</a>
+
+        <div className=''>
+          <img src="" alt="voidTask" />
+          Você ainda não tem tarefas cadastradas
+          Crie tarefas e organize seus itens a fazer
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import styles from './Logo.module.css';
+
+export function Logo() {
+    return(
+        <div className={styles.title}>to do</div>
+    );
+}
